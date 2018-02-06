e18f5e9cfc26f7e513a56ccceb1576525e6de87c
Oladele Femi <profemzy@gmail.com>
2018-02-06 12:27:46 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,433 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="InfoTitans - Tech Giants">
+    <meta name="author" content="Femi Oladele  - profemzy">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <title> InfoTitans - Tech Giants</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/icomoon.css">
+    <link href="assets/css/animate-custom.css" rel="stylesheet">
+
+
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/modernizr.custom.js"></script>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body data-spy="scroll" data-offset="0" data-target="#navbar-main">
+
+
+<div id="navbar-main">
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse">
+
+        <div class="container">
+            <div class="navbar-header">
+
+                <img style="margin-top: 4px" src="assets/img/logo1.png" />
+
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon icon-shield" style="font-size:30px; color:#3498db;"></span>
+                </button>
+
+
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="#home" class="smoothScroll">Home</a></li>
+                    <li> <a href="#about" class="smoothScroll"> About</a></li>
+                    <li> <a href="#services" class="smoothScroll"> Services</a></li>
+                    <li> <a href="#team" class="smoothScroll"> Team</a></li>
+                    <li> <a href="#portfolio" class="smoothScroll"> Portfolio</a></li>
+                   <li> <a href="http://blog.infotitans.com" class="smoothScroll" target="_blank"> Blog</a></li>
+                   <li> <a href="https://buildasaasappwithflask.com" class="smoothScroll" target="_blank"> Learn Flask</a></li>
+                  
+
+                    <li> <a href="#contact" class="smoothScroll"> Contact</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </div>
+</div>
+
+
+
+<!-- ==== HEADERWRAP ==== -->
+<div id="headerwrap" id="home" name="home">
+    <header class="clearfix">
+        <h1><span class="icon icon-shield"></span></h1>
+        <p>The Tech Giants.</p>
+
+    </header>
+</div><!-- /headerwrap -->
+
+<!-- ==== GREYWRAP ==== -->
+<div id="greywrap">
+    <div class="row">
+        <div class="col-lg-4 callout">
+            <span class="icon icon-mobile-2"></span>
+            <h2>Mobile & Web Apps</h2>
+            <p>We build custom made native mobile apps to suit your business and personal needs. </p>
+        </div><!-- col-lg-4 -->
+
+        <div class="col-lg-4 callout">
+            <span class="icon icon-briefcase-2"></span>
+            <h2>Tech Solution Providers</h2>
+            <p>We provide I.T Technical Support, both On Site and Online and we are just a mail away. </p>
+        </div><!-- col-lg-4 -->
+
+        <div class="col-lg-4 callout">
+            <span class="icon icon-laptop"></span>
+            <h2>Training & Internship</h2>
+            <p>
+              Trying to build sites with October? Get the full eBook with Bonus material <a href="https://leaderinternet.com/products/october-cms-up-and-running" targer="_blank">October CMS - Up and Running.</a>
+               
+         	</p>
+        </div><!-- col-lg-4 -->
+    </div><!-- row -->
+</div><!-- greywrap -->
+
+<!-- ==== ABOUT ==== -->
+<div class="container" id="about" name="about">
+    <div class="row white">
+        <br>
+        <h1 class="centered">GET TO KNOW US</h1>
+        <hr>
+
+        <div class="col-lg-6">
+            <p>
+                For a couple of years Information Titans(InfoTitans) has been involved in the design, implementation and support of diverse range of information technology systems and solutions.
+
+                We deploy our products and services to suit your business needs, we are a team of dynamic professionals focused on offering effective and reliable I.T solutions that empower organizational productivity.
+                We really are "Tech Giants".
+            </p>
+        </div><!-- col-lg-6 -->
+
+        <div class="col-lg-6">
+            <p>  We have not only deployed solutions to suit client needs over the years we have taken steps further to eliminate any barrier between us and clients by implement a very robust online support system to facilitate ease of operation for our clients regardless of their locations, we have also trained countless numbers of interns who have
+                 grown to become veterans in the industry and we are always happy to do these things over and over again.
+            </p>
+        </div><!-- col-lg-6 -->
+    </div><!-- row -->
+</div><!-- container -->
+
+<!-- ==== SECTION DIVIDER1 -->
+<section class="section-divider textdivider divider1">
+    <div class="container">
+        <h1>TECH SUPPORT WITHOUT BOUNDARIES</h1>
+        <hr>
+        <p>We have taken steps further to eliminate any barrier between us and clients by implement a very robust online support system to facilitate ease of operation for our clients regardless of their locations</p>
+    </div><!-- container -->
+</section><!-- section -->
+
+
+<!-- ==== SERVICES ==== -->
+<div class="container" id="services" name="services">
+    <br>
+    <br>
+    <div class="row">
+        <h2 class="centered">CLOUD INFRASTRUCTURE & VOIP CONSULTANTS.</h2>
+        <hr>
+        <br>
+        <div class="col-lg-offset-2 col-lg-8">
+            <p>
+                We provide VOIP Consultancy and deployment services to clients intending to defy old ways of communication and provide access to services by integrating enhanced VOIP services to automate business processes.
+            </p>
+            <p>
+                We have supported clients in deployment of cloud infrastructure in Europe, North America where they have seamless connectivity to their business resources hosted on these platforms
+            </p>
+
+        </div><!-- col-lg -->
+    </div><!-- row -->
+
+    <div class="row">
+        <h2 class="centered">CROSS PLATFORM MOBILE & WEB APPLICATION DEVELOPMENT</h2>
+        <hr>
+        <br>
+        <div class="col-lg-offset-2 col-lg-8">
+            <img class="img-responsive" src="assets/img/iphone.png" alt="">
+        </div><!-- col -->
+    </div><!-- row -->
+</div><!-- container -->
+
+
+<!-- ==== SECTION DIVIDER2 -->
+<section class="section-divider textdivider divider2">
+    <div class="container">
+        <h1>TECH GIST AND RECENT TECHNOLOGY TRENDS</h1>
+        <hr>
+        <p>
+            As we learn new trends and patterns in technology, we share and discuss our findings with as many people as are willing to hear us.
+        </p>
+    </div><!-- container -->
+</section><!-- section -->
+
+<!-- ==== TEAM MEMBERS ==== -->
+<div class="container" id="team" name="team">
+    <br>
+    <div class="row white centered">
+        <h1 class="centered">MEET THE PIONEERS</h1>
+        <hr>
+        <br>
+        <br>
+        <div class="col-lg-3 centered">
+            <img class="img img-circle" src="assets/img/team/prof.png" height="120px" width="120px" alt="">
+            <br>
+            <h4><b>Oladele Femi</b></h4>
+            <a href="https://twitter.com/profemzy" class="icon icon-twitter"></a>
+            <a href="https://web.facebook.com/profemzy" class="icon icon-facebook"></a>
+            <a href="#" class="icon icon-flickr"></a>
+            <p>Femi is an IT Consultant with keen interest in application development(Java, C#, Python, PHP), network infrastructure establishment and maintenance, working with clients from a wide range of industries, he fully understands client objectives when working on a project, large or small.</p>
+        </div><!-- col-lg-3 -->
+
+
+
+    </div><!-- row -->
+</div><!-- container -->
+
+<!-- ==== GREYWRAP ==== -->
+<div id="greywrap">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 centered">
+                <img class="img-responsive" src="assets/img/macbook.png" align="">
+            </div>
+            <div class="col-lg-4">
+                <h2>We Are Hiring Interns</h2>
+                <p>Do you want to be one of us? Sure you want, because we are an awesome team!. Here we work hard every day to create life transforming applications.</p>
+                <p><a class="btn btn-success">Contact Us</a></p>
+            </div>
+        </div><!-- row -->
+    </div>
+    <br>
+    <br>
+</div><!-- greywrap -->
+
+<!-- ==== SECTION DIVIDER3 -->
+<section class="section-divider textdivider divider3">
+    <div class="container">
+        <h1>ONCE WE CAN SEE IT, WE CAN CREATE IT!</h1>
+        <hr>
+
+    </div><!-- container -->
+</section><!-- section -->
+
+<!-- ==== PORTFOLIO ==== -->
+<div class="container" id="portfolio" name="portfolio">
+    <br>
+    <div class="row">
+        <br>
+        <h1 class="centered">WE CREATE COOL STUFF</h1>
+        <hr>
+        <br>
+        <br>
+    </div><!-- /row -->
+    <div class="container">
+        <div class="row">
+
+            <!-- PORTFOLIO IMAGE 1 -->
+            <div class="col-md-4 ">
+                <div class="grid mask">
+                    <figure>
+                        <img class="img-responsive" src="assets/img/portfolio/folio01.jpg" alt="">
+                        <figcaption>
+                            <h5>WEB APPS</h5>
+
+                        </figcaption><!-- /figcaption -->
+                    </figure><!-- /figure -->
+                </div><!-- /grid-mask -->
+            </div><!-- /col -->
+
+
+            <!-- MODAL SHOW THE PORTFOLIO IMAGE. In this demo, all links point to this modal. You should create
+                 a modal for each of your projects. -->
+
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                            <h4 class="modal-title">Project Title</h4>
+                        </div>
+                        <div class="modal-body">
+                            <p><img class="img-responsive" src="assets/img/portfolio/folio01.jpg" alt=""></p>
+                            <p>This project was crafted for Some Name corp. Detail here a little about your job requirements and the tools used. Tell about the challenges faced and what you and your team did to solve it.</p>
+                            <p><b><a href="#">Visit Site</a></b></p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div><!-- /.modal-content -->
+                </div><!-- /.modal-dialog -->
+            </div><!-- /.modal -->
+
+
+            <!-- PORTFOLIO IMAGE 2 -->
+            <div class="col-md-4">
+                <div class="grid mask">
+                    <figure>
+                        <img class="img-responsive" src="assets/img/portfolio/folio02.jpg" alt="">
+                        <figcaption>
+                            <h5>CONSULTANCY</h5>
+
+                        </figcaption><!-- /figcaption -->
+                    </figure><!-- /figure -->
+                </div><!-- /grid-mask -->
+            </div><!-- /col -->
+
+            <!-- PORTFOLIO IMAGE 3 -->
+            <div class="col-md-4">
+                <div class="grid mask">
+                    <figure>
+                        <img class="img-responsive" src="assets/img/portfolio/folio03.jpg" alt="">
+                        <figcaption>
+                            <h5>MOBILE APPS</h5>
+
+                        </figcaption><!-- /figcaption -->
+                    </figure><!-- /figure -->
+                </div><!-- /grid-mask -->
+            </div><!-- /col -->
+        </div><!-- /row -->
+
+        </div><!-- /row -->
+        <br>
+        <br>
+    </div><!-- /row -->
+</div><!-- /container -->
+
+
+<div class="container" id="contact" name="contact">
+    <div class="row">
+        <br>
+        <h1 class="centered">THANKS FOR VISITING US</h1>
+        <hr>
+        <br>
+        <br>
+        <div class="col-lg-4">
+            <h3>Contact Information</h3>
+            <p><span class="icon icon-home"></span> Suite 41, Adamasingba Shopping Complex, Ibadan, Nigeria<br/>
+                <span class="icon icon-phone"></span> +234 803 357 4778 <br/>
+                <span class="icon icon-mobile"></span> +234 706 217 5906 <br/>
+                <span class="icon icon-envelop"></span> <a href="#"> info@infotitans.com</a> <br/>
+                <span class="icon icon-twitter"></span> <a href="#"> @profemzy </a> <br/>
+                <span class="icon icon-facebook"></span> <a href="https://www.facebook.com/InfoTitans-139937389377868/?ref=hl"> InfoTitans</a> <br/>
+            </p>
+        </div><!-- col -->
+
+        <div class="col-lg-4">
+            <h3>Need Info?</h3>
+            <p>
+                Feel free to contact us via the form below.
+            </p>
+            <p>
+            <form class="form-horizontal" role="form" method="post">
+                <div class="form-group">
+                    <label for="inputEmail1" class="col-lg-4 control-label"></label>
+                    <div class="col-lg-10">
+                        <input name="email" type="email" class="form-control" id="inputEmail1" placeholder="Email">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="text1" class="col-lg-4 control-label"></label>
+                    <div class="col-lg-10">
+                        <input name="name" type="text" class="form-control" id="text1" placeholder="Your Name">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="message" class="col-lg-4 control-label"></label>
+                    <div class="col-lg-10">
+                        <textarea name="message" id="message" class="form-control" placeholder="Message">
+
+                        </textarea>
+                     </div>
+                </div>
+               <div class="form-group">
+                 <div class="col-lg-10">
+                   <input name="sum" type="text" class="form-control" id="textSum" placeholder="10 Multiplied by 150">
+                 </div>  
+              </div>   
+                <div class="form-group">
+                    <div class="col-lg-10">
+                        <button type="submit" class="btn btn-success">Send</button>
+                    </div>
+                </div>
+            </form><!-- form -->
+
+    <?php
+        $name=$_REQUEST['name'];
+        $email=$_REQUEST['email'];
+        $message=$_REQUEST['message'];
+          $sum = $_REQUEST['sum'];
+
+        if (($name=="")||($email=="")||($message=="")) {
+
+        }
+        else {
+          
+            $from="From: $name<$email>\r\nReturn-path: $email";
+            $subject="Message sent using your contact form";
+          if($sum = 1500){
+            //mail("profemzy@infotitans.com", $subject, $message, $from);
+          }
+          else {
+           echo "Invalid Sum Supplied!"; 
+          }
+            echo "<br/>";
+            echo "<pre>";
+            echo "Email sent!";
+            echo "</pre>";
+          
+          
+        }
+    ?>
+
+            </p>
+        </div><!-- col -->
+
+        <div class="col-lg-4">
+            <h3>Online Tech Support</h3>
+            <p>
+                For real-time online tech support, send email to profemzy@infotitans.com
+            </p>
+        </div><!-- col -->
+
+    </div><!-- row -->
+
+</div><!-- container -->
+
+<div id="footerwrap">
+    <div class="container">
+        <h4><a href="http://infotitans.com">InfoTitans</a> - Copyright <?php echo date("Y"); ?></h4>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+
+
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/retina.js"></script>
+<script type="text/javascript" src="assets/js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="assets/js/smoothscroll.js"></script>
+<script type="text/javascript" src="assets/js/jquery-func.js"></script>
+</body>
+</html>
